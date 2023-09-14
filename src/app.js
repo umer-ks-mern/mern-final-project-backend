4523daa95e10bc1bcc7455be09a35ccc5794498f
Umer <aehtesham086@gmail.com>
2023-09-14 16:28:22 +0500

    user endpoint created

--- src/app.js
+++ src/app.js
@@ -9,6 +9,10 @@ app.get("/", (req, res) => {
   return res.json({ message: "E-Commerce Backend is Working Fine" });
 });
 
+app.get("/user", (req,res) => {
+  res.send("<h1>Users</h1>")
+})
+
 app.listen(3301, () => {
     console.log('E-Commrce ka Backend Server Port: 3301 py active ha');
 })
